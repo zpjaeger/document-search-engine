e339487ad81f10c090e1d63a1cfe6dee5ac85813
zjaeger01 <zjaeger@asu.edu>
2025-09-21 13:40:21 -0700

    Project scaffolding

---
+++ src/routes.py
@@ -0,0 +1,19 @@
+#1.  **Indexing & Searching:** Your program should parse the document `body`, build an index, and accept a query string to find relevant documents.
+#2.  **TF-IDF Ranking:** Search results must be ranked and sorted according to their TF-IDF (Term Frequency-Inverse Document Frequency) score.
+#3.  **Code Quality & Documentation:** Write clean, readable code. Provide a `README.md` with instructions on how to build and run your project. Be prepared to discuss your development process and design choices.
+#4.  **Testing:** Include unit tests that cover the implementation details of your search engine (e.g., indexing, scoring, ranking).
+
+# 1. Retrieve documents from local data source in data/documents.json
+import json
+import uvicorn
+import os
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
